27ea50a4c6009f5ffe5efa39106cabc7c7b666b6
CrashPanda <yolouki@hotmail.com>
2021-12-23 23:10:35 +0000

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    console.log( "ready!" );
+  //alert('this is an example alert');
+  $('.carousel').carousel({
+  interval: 2000
+})
+  
+});
+
 jQuery(document).ready(function() {
  
   /*
